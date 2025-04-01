6c5727c289ea9d1cea6dd5f91b2ff32c8f5f6135
Son <nguyenxuansondeptrai@gmail.com>
2025-04-02 01:28:17 +0700

    UI: Complete Home Screen (without navigation)

---
+++ app/src/main/java/com/mobileapplication/mobileassignment1/HomeActivity.java
@@ -0,0 +1,12 @@
+package com.mobileapplication.mobileassignment1;  // Adjust this to your package name
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);  // Create a corresponding layout file (activity_home.xml)
+    }
+}
---
+++ app/src/main/java/com/mobileapplication/mobileassignment1/MainView1Activity.java
@@ -0,0 +1,11 @@
+package com.mobileapplication.mobileassignment1;
+
+public class MainView1Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // If your layout file is mainview1.xml, reference it here:
+        setContentView(R.layout.mainview1);
+    }
+}
+
